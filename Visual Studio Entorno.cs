6288a1db399dab441e46c8a254e2ecb6d729bec7
ALej04 <113586645+ALej04@users.noreply.github.com>
2022-09-29 09:50:22 +0200

    Add files via upload

---
+++ Visual Studio Entorno.cs
@@ -0,0 +1,14 @@
+float n1;
+float n2;
+Console.Write("Primer número: ");
+n1 = float.Parse(Console.ReadLine());
+Console.Write("Segundo número: ");
+n2 = float.Parse(Console.ReadLine());
+if (n1 > n2)
+{
+    Console.WriteLine("El número mayor es: " + n1);
+}
+else
+{
+    Console.WriteLine("El número mayor es: " + n2);
+}
